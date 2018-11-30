aa423d9a34da27b4ee7281e3777b315f180973e0
Pontus Persson <pontus.alexander@gmail.com>
2018-11-30 10:45:23 +0100

    Refactor test format.

--- challenges/build.js
+++ challenges/build.js
@@ -1,3 +1,4 @@
+const uuidv4 = require('uuid/v4');
 const path = require('path');
 const fs = require('fs');
 
@@ -6,11 +7,12 @@ const configPath = path.resolve(path.join('.', DIR, 'config.json'));
 const config = require(configPath);
 
 const output = {
-  tests: [],
+  questions: [],
 };
 
 function loadSnippet(path) {
   return {
+    id: uuidv4(),
     type: path.split('.').pop(),
     content: fs.readFileSync(path, 'utf8').trim(),
   };
@@ -21,12 +23,14 @@ for (const testName of config.tests) {
   const good = path.join(testPath, 'good.js');
   const bad = path.join(testPath, 'bad.js');
 
-  const test = {
-    good: loadSnippet(good),
-    bad: loadSnippet(bad),
+  const question = {
+    answers: [
+      loadSnippet(good),
+      loadSnippet(bad),
+    ],
   };
 
-  output.tests.push(test);
+  output.questions.push(question);
 }
 
 process.stdout.write(JSON.stringify(output));
