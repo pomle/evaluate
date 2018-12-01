4b4334a04a44564bcadae0b8a040cb2ce23ed987
Pontus Alexander <pontus.alexander@gmail.com>
2018-12-01 08:56:58 +0100

    Intro page.

---
+++ web-client/components/widgets/TestStarter/index.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      testRef: null
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="test-starter">
+    <table>
+      <tr>
+        <th>Test Reference</th>
+        <td>
+          <input v-model="testRef"/>
+        </td>
+      </tr>
+    </table>
+
+    <button class="primary">Start</button>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,65 +1,29 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        evaluate
-      </h1>
-      <h2 class="subtitle">
-        My superior Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
-</template>
-
 <script>
-import Logo from '~/components/Logo.vue';
+import TestStarter from '~/components/widgets/TestStarter';
 
 export default {
   components: {
-    Logo
-  }
+    TestStarter,
+  },
 };
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<template>
+  <div class="intro-page">
+    <section class="intro">
+      <h1>Welcome to Evaluate</h1>
+    </section>
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    <section class="take-test">
+      <h2>Take a Test</h2>
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+      <TestStarter/>
+    </section>
+  </div>
+</template>
 
-.links {
-  padding-top: 15px;
-}
+<style lang="less">
+  .intro-page {
+
+  }
 </style>
