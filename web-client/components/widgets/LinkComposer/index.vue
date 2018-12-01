119b6fd3a10613ec606b4c8577aaf085933fd5aa
Pontus Alexander <pontus.alexander@gmail.com>
2018-12-01 10:27:45 +0100

    Add link composer.

---
+++ web-client/components/widgets/LinkComposer/index.vue
@@ -0,0 +1,49 @@
+<script>
+import { random } from '~/lib/random';
+
+export default {
+  data() {
+    return {
+      resultId: random(12),
+      testRef: 'cae844ce-f487-11e8-8f10-1040f388afa6'
+    };
+  },
+
+  computed: {
+    link() {
+      const link = `/test?testId=${encodeURIComponent(
+        this.testRef
+      )}&resultId=${encodeURIComponent(this.resultId)}`;
+      return link;
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="link-page">
+    <table>
+      <tbody>
+        <tr>
+          <th>Test Reference</th>
+          <td>
+            <input v-model="testRef">
+          </td>
+        </tr>
+        <tr>
+          <th>Result Id</th>
+          <td>
+            <input v-model="resultId">
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="link">
+      <a :href="link">{{ link }}</a>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+</style>
---
+++ web-client/pages/link.vue
@@ -0,0 +1,23 @@
+<script>
+import LinkComposer from '~/components/widgets/LinkComposer';
+
+export default {
+  components: {
+    LinkComposer
+  }
+};
+</script>
+
+<template>
+  <div class="link-page">
+    <section class="intro">
+      <h1>Create link</h1>
+    </section>
+
+    <section class="take-test">
+      <h2>Create a test link</h2>
+
+      <LinkComposer/>
+    </section>
+  </div>
+</template>
