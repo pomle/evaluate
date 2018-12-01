137b70f541e2af424fafd510235eb69dcbbde449
Pontus Alexander <pontus.alexander@gmail.com>
2018-12-01 09:38:13 +0100

    TestStarter works.

--- web-client/components/widgets/TestStarter/index.vue
+++ web-client/components/widgets/TestStarter/index.vue
@@ -1,9 +1,30 @@
 <script>
+import { random } from '~/lib/random';
+
 export default {
   data() {
     return {
       testRef: null
     };
+  },
+
+  methods: {
+    start() {
+      const testId = this.testRef;
+      const sessionId = random(12);
+      const resultId = random(12);
+
+      this.$store.dispatch('session/loadTest', {
+        sessionId,
+        testId,
+        resultId
+      });
+
+      this.$router.push({
+        name: 'session-sessionId',
+        params: { sessionId }
+      });
+    }
   }
 };
 </script>
@@ -11,15 +32,19 @@ export default {
 <template>
   <div class="test-starter">
     <table>
-      <tr>
-        <th>Test Reference</th>
-        <td>
-          <input v-model="testRef">
-        </td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Test Reference</th>
+          <td>
+            <input v-model="testRef">
+          </td>
+        </tr>
+      </tbody>
     </table>
 
-    <button class="primary">Start</button>
+    <button
+      class="primary"
+      @click="start">Start</button>
   </div>
 </template>
 
