f1bc26f9d950aabf4edf17efeb59f71327b31b73
Pontus Persson <pontus.alexander@gmail.com>
2018-12-06 14:44:17 +0100

    Add Busy layout.

---
+++ web-client/layouts/busy.vue
@@ -0,0 +1,41 @@
+<script>
+import logo from '~/assets/img/justarrived-logo.png';
+
+export default {
+  data() {
+    return {
+      logo
+    };
+  }
+};
+</script>
+
+<template>
+  <div class="busy-layout">
+    <div class="logo">
+      <img :src="logo">
+    </div>
+
+    <div class="content">
+      <nuxt/>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.busy-layout {
+  max-width: 600px;
+  margin: 0 auto;
+
+  .logo {
+    img {
+      display: block;
+      margin: 50px auto;
+    }
+  }
+
+  .content {
+    text-align: center;
+  }
+}
+</style>
