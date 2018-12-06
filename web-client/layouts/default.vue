01acdc2809556dcdfca779f88176cf3609e7c973
Pontus Persson <pontus.alexander@gmail.com>
2018-12-06 13:24:14 +0100

    Show logo on default layout.

--- web-client/layouts/default.vue
+++ web-client/layouts/default.vue
@@ -1,11 +1,36 @@
+<script>
+import logo from '~/assets/img/justarrived-logo.png';
+
+export default {
+  data() {
+    return {
+      logo
+    };
+  }
+};
+</script>
+
 <template>
   <div class="default">
+    <header>
+      <img 
+        :src="logo" 
+        class="logo">
+    </header>
+
     <nuxt/>
   </div>
 </template>
 
 <style lang="less">
 #__layout > .default {
+  & > header {
+    .logo {
+      height: 40px;
+      margin: 10px;
+    }
+  }
+
   section {
     max-width: 1000px;
     margin: 2em auto;
