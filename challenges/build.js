d3af46e8133e741d71242fb2fc43b54d7b7e2507
Pontus Persson <pontus.alexander@gmail.com>
2018-12-07 13:56:09 +0100

    Build solution as well.

--- challenges/build.js
+++ challenges/build.js
@@ -1,16 +1,27 @@
 #! /usr/bin/env node
 const uuidv4 = require('uuid/v4');
+const crypto = require('crypto')
 const path = require('path');
 const fs = require('fs');
 const {markdown} = require('markdown');
 
 const DIR = process.argv[2];
+const buildPath = path.resolve(path.join(__dirname, 'build'));
 const configPath = path.resolve(path.join('.', DIR, 'config.json'));
 const config = require(configPath);
 
-const output = {
+const test = {
   questions: [],
 };
+
+const solution = {
+  answers: {},
+};
+
+function encode(data) {
+  const json = JSON.stringify(data);
+  return Buffer.from(json).toString('base64');
+}
 
 function loadSnippet(path) {
   return {
@@ -35,19 +46,30 @@ for (const testName of config.tests) {
   const docPath = path.join(testPath, 'README.md');
   const doc = markdown.parse(fs.readFileSync(docPath, 'utf8'));
 
+  const correct = loadSnippet(goodPath);
+  const wrong = loadSnippet(badPath);
+
   const question = {
     id: uuidv4(),
     statement: parseStatement(doc),
     answers: [
-      loadSnippet(goodPath),
-      loadSnippet(badPath),
+      correct,
+      wrong,
     ],
   };
 
-  output.questions.push(question);
+  solution.answers[question.id] = correct.id;
+
+  test.questions.push(question);
 }
 
-const json = JSON.stringify(output);
-const encoded = Buffer.from(json).toString('base64');
+const testData = encode(test);
+const solutionData = encode(solution);
 
-process.stdout.write(encoded);
+const hasher = crypto.createHash('sha1');
+hasher.update(testData);
+const hash = hasher.digest('hex');
+const name = Buffer.from(hash).toString('base64').slice(0, 12);
+
+fs.writeFileSync(buildPath + '/' + name, testData, 'utf8');
+fs.writeFileSync(buildPath + '/' + `${name}.solution`, solutionData, 'utf8');
