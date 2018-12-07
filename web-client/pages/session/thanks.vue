360750e8e878f66f8eab0eccfe0f1c8c8f448468
Pontus Persson <pontus.alexander@gmail.com>
2018-12-07 08:40:07 +0100

    Add thank you page.

---
+++ web-client/pages/session/thanks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="thanks-page">
+    <h1>Thank you!</h1>
+
+    <p>
+      That's all. We hope you enjoyed doing the test.
+    </p>
+  </div>
+</template>
+
+<style lang="less">
+.thanks-page {
+  margin: 5em 0;
+  text-align: center;
+
+  h1 {
+    margin: 2em 0;
+  }
+}
+</style>
