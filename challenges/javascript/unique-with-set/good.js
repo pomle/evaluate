57f3aa64fea051c2f65a5b6b70995edff71e5f31
Pontus Persson <pontus.alexander@gmail.com>
2018-12-07 11:34:39 +0100

    Fix unique with set.

--- challenges/javascript/unique-with-set/good.js
+++ challenges/javascript/unique-with-set/good.js
@@ -1,13 +1,5 @@
 function uniqify(numbers) {
-  const unique = [];
-  const mem = Object.create(null);
-  for (const number of numbers) {
-    if (!mem[number]) {
-      unique.push(number);
-      mem[number] = true;
-    }
-  }
-  return unique;
+  return [...new Set(numbers)];
 }
 
 module.exports = uniqify;
